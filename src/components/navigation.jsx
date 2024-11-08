f9aa71dd224373257c71f274e4c2b6a1ab622878
siddharth <siddharthrbhetariya@gmail.com>
2024-11-08 17:30:11 +0530

    logo added

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            React Landing Page
+          <img src="/img/logo.png"alt="Logo"className="navbar-logo" />
           </a>{" "}
         </div>
 
